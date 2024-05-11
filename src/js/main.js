a3adcb66854c1f8154d648894cad8a6dd1dc0dca
Anton <erikssonanton94@gmail.com>
2024-05-11 04:10:27 +0200

    Läser in menydatan och skriver ut den på förstasidsan

--- src/js/main.js
+++ src/js/main.js
@@ -11,4 +11,35 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
         editMenuEl.style.display = "none";
     }
+
+    getData();
 });
+
+//Hämtar in data från databasen
+async function getData() {
+    const url = "https://projektapi-backend2.onrender.com/api/menu";
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const menuContainer = document.getElementById("menuContainer");
+        
+
+        if (response.ok) {
+            //Loopa igenom datan och skapa article- element för varje jobberfrenhet
+            data.forEach(menuItem => {
+                const article = document.createElement("article");
+                article.dataset._id = menuItem._id; //Ger articlen samma ID som jobberfarenheten
+                article.innerHTML = `
+                <h3>${menuItem.name}</h3>
+                <p>${menuItem.description}</p>
+                <p><strong>Pris: ${menuItem.price}</strong></p>
+                `;
+                menuContainer.appendChild(article);
+            });
+        }
+
+    } catch (error) {
+        console.error("Något gick fel: ", error);
+    }
+}
